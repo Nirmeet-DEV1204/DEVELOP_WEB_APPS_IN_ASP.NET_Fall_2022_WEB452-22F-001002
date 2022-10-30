a88cef6ccadaed4e8f3b20caab1464ab6570217f
Nirmeet Pansuriya <96394155+Nirmeet-DEV1204@users.noreply.github.com>
2022-10-30 16:45:23 -0400

    Add files via upload
    Project start

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AnddyBookstores.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
